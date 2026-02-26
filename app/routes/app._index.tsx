af6809cce260ec27b017e3cb2e724322485b2f7c
lucasliin <2376909468@qq.com>
2026-02-26 10:44:24 +0800

    🚧 work in session token demo

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -4,15 +4,23 @@ import type {
   HeadersFunction,
   LoaderFunctionArgs,
 } from "react-router";
-import { useFetcher } from "react-router";
+import { useFetcher, useLoaderData } from "react-router";
 import { useAppBridge } from "@shopify/app-bridge-react";
 import { authenticate } from "../shopify.server";
 import { boundary } from "@shopify/shopify-app-react-router/server";
+import { getSessionToken } from "@shopify/app-bridge/utilities";
+import createApp from "@shopify/app-bridge";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   await authenticate.admin(request);
 
-  return null;
+  const url = new URL(request.url);
+  const host = url.searchParams.get("host");
+  if (!host) {
+    throw new Error("Missing host parameter in URL");
+  }
+
+  return { host, apiKey: process.env.SHOPIFY_API_KEY };
 };
 
 export const action = async ({ request }: ActionFunctionArgs) => {
@@ -86,6 +94,7 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 export default function Index() {
   const fetcher = useFetcher<typeof action>();
+  const { host, apiKey } = useLoaderData<typeof loader>();
 
   const shopify = useAppBridge();
   const isLoading =
@@ -97,6 +106,26 @@ export default function Index() {
       shopify.toast.show("Product created");
     }
   }, [fetcher.data?.product?.id, shopify]);
+
+  const getAppSessionToken = async () => {
+    console.log("host ============================= ", host);
+    console.log("api key ============================= ", apiKey);
+    if (!apiKey || !host) {
+      return;
+    }
+    const app = createApp({ apiKey, host });
+    const token = await getSessionToken(app);
+    console.log("session token ============================= ", token);
+    const result = await fetch(`http://localhost:9998/protected/ping`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`,
+      },
+    });
+    const data = await result.json();
+    console.log("data ============================= ", data);
+  };
 
   const generateProduct = () => fetcher.submit({}, { method: "POST" });
 
@@ -145,6 +174,9 @@ export default function Index() {
             {...(isLoading ? { loading: true } : {})}
           >
             Generate a product
+          </s-button>
+          <s-button onClick={getAppSessionToken}>
+            get app session token
           </s-button>
           {fetcher.data?.product && (
             <s-button
