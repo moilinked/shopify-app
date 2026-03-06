57639179aacafb598bf31dfd2f8d8487a6bda87c
lucasliin <2376909468@qq.com>
2026-03-06 09:35:52 +0800

    🚧work in hamc job and shopify access scopes

--- app/routes/customapi.child.tsx
+++ app/routes/customapi.child.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunctionArgs } from 'react-router'
+import { ActionFunctionArgs, LoaderFunctionArgs } from 'react-router'
 import { authenticate } from '../shopify.server'
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -6,7 +6,14 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   return {}
 }
 
-export const action = async () => {
+export const action = async ({ request }: ActionFunctionArgs) => {
   console.log('触发 child')
+  const url = new URL(request.url)
+  const hmac = url.searchParams.toString()
+  const result = await fetch('http://localhost:9998/app/ping', {
+    headers: { 'Content-Type': 'application/json', hmac },
+  })
+  const data = await result.json()
+  console.log('data ============================= ', data)
   return { message: 'ok' }
 }
